6502acaddb0f102218aad97c4c124ffc5206138f
Ferytell <darkstar2709@gmail.com>
2023-03-14 20:35:38 +0700

    sdfsf

---
+++ let.js
@@ -0,0 +1,24 @@
+function scoreChecker(score) {
+    let result;
+  
+    // TODO
+  if(score >= 90){
+    result = 'Selamat! Anda mendapatkan nilai A.';
+  }else if(score >= 80){
+    result = 'Anda mendapatkan nilai B.';
+  }else if(score >= 70){
+    result = 'Anda mendapatkan nilai C.';
+  }else if(score >= 60){
+    result = 'Anda mendapatkan nilai D.';
+  }else{
+    result = 'Anda mendapatkan nilai E.';
+  }
+  
+    // Jangan hapus kode ini
+    return result;
+  }
+  console.log(scoreChecker(92));
+  /**
+   * Jangan hapus kode di bawah ini
+   */
+  module.exports = scoreChecker;
--- main.go
+++ main.go
@@ -1,48 +1,88 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"math/rand"
+// 	"time"
+// )
+
+// func goroutine1(data interface{}, done chan bool) {
+// 	for i := 0; i < 4; i++ {
+// 		fmt.Printf("GOROUTINE 1: data=%v, loop=%d\n", data, i+1)
+// 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+// 	}
+// 	done <- true
+// }
+
+// func goroutine2(data interface{}, done chan bool) {
+// 	for i := 0; i < 4; i++ {
+// 		fmt.Printf("GOROUTINE 2: data=%v, loop=%d\n", data, i+1)
+// 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+// 	}
+// 	done <- true
+// }
+
+// func main() {
+// 	done1 := make(chan bool)
+// 	done2 := make(chan bool)
+
+// 	data1 := "hello"
+// 	data2 := 42
+
+// 	go goroutine1(data1, done1)
+// 	go goroutine2(data2, done2)
+
+// 	<-done1
+// 	<-done2
+
+// 	fmt.Println("Both goroutines have finished")
+// }
+
 package main
 
 import (
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 )
 
-func printData(data interface{}, num int, c chan int) {
+var mutex = &sync.Mutex{}
+
+func goroutine1(data interface{}, done chan bool) {
 	for i := 0; i < 4; i++ {
+		mutex.Lock()
+		fmt.Printf("GOROUTINE 1: data=%v, loop=%d\n", data, i+1)
+		mutex.Unlock()
 		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
-		fmt.Printf("Data %d: %v, Loop %d\n", num, data, i+1)
 	}
-	c <- num
+	done <- true
+}
+
+func goroutine2(data interface{}, done chan bool) {
+	for i := 0; i < 4; i++ {
+		mutex.Lock()
+		fmt.Printf("GOROUTINE 2: data=%v, loop=%d\n", data, i+1)
+		mutex.Unlock()
+		time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+	}
+	done <- true
 }
 
 func main() {
-	c1 := make(chan int)
-	c2 := make(chan int)
+	done1 := make(chan bool)
+	done2 := make(chan bool)
 
-	data1 := []interface{}{"foo", 123, true}
-	data2 := []interface{}{"bar", 456, false}
+	data1 := "hello"
+	data2 := 42
 
-	go func() {
-		for i := 0; i < 1; i++ {
-			randIndex := rand.Intn(len(data1))
-			go printData(data1[randIndex], 1, c1)
-		}
-	}()
+	go goroutine1(data1, done1)
+	go goroutine2(data2, done2)
 
-	go func() {
-		for i := 0; i < 1; i++ {
-			randIndex := rand.Intn(len(data2))
-			go printData(data2[randIndex], 2, c2)
-		}
-	}()
+	<-done1
+	<-done2
 
-	for i := 0; i < 8; i++ {
-		select {
-		case <-c1:
-			fmt.Println("Data 1 done")
-		case <-c2:
-			fmt.Println("Data 2 done")
-		}
-	}
+	fmt.Println("Both goroutines have finished")
 }
 
 /*
